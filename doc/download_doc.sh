9fb5c40a64b8184e9c7bb257e28c4e1f7948cc97
AndreaMarcer <marcer.andrea@gmail.com>
2024-01-11 23:14:14 +0100

    Add script to download all the documents

---
+++ doc/download_doc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# PICO
+wget -P ${ARC_DIR}/doc/pico/ https://datasheets.raspberrypi.com/pico/pico-datasheet.pdf
+wget -P ${ARC_DIR}/doc/pico/ https://datasheets.raspberrypi.com/rp2040/rp2040-datasheet.pdf
+wget -P ${ARC_DIR}/doc/pico/ https://datasheets.raspberrypi.com/pico/raspberry-pi-pico-c-sdk.pdf
+wget -P ${ARC_DIR}/doc/pico/ https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf
+
+# MPU6050
+wget -P ${ARC_DIR}/doc/MPU6050/ https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Datasheet1.pdf
+wget -P ${ARC_DIR}/doc/MPU6050/ https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Register-Map1.pdf
