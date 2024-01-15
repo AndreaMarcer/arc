bcf8349577006aeea6c971bb1efacb0b1f6234b6
AndreaMarcer <marcer.andrea@gmail.com>
2024-01-15 22:59:06 +0100

    Main clean up

--- arc/src/main.cpp
+++ arc/src/main.cpp
@@ -14,38 +14,17 @@
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
-#include "hardware/adc.h"
 #include "math.h"
 
 #include "hardware/gpio.h"
 
 #include "sensors/MPU6050.hpp"
 
-uint counter = 0;
-
-arc::sensors::MPU6050 *p_mpu6050{ nullptr };
-float g_acc[3];
-
-void gpio_callback(uint gpio, uint32_t events)
-{
-	counter++;
-}
-
 int main()
 {
 	stdio_init_all();
-#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || \
-	!defined(PICO_DEFAULT_I2C_SCL_PIN)
-#warning i2c/mpu6050_i2c example requires a board with I2C pins
-	puts("Default I2C pins were not defined");
-	return 0;
-#else
+
 	printf("\n\n\n\nHello, MPU6050! Reading raw data from registers...\n");
-
-	const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-
-	gpio_init(LED_PIN);
-	gpio_set_dir(LED_PIN, GPIO_OUT);
 
 	// This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
 	i2c_init(i2c_default, 400 * 1000);
@@ -58,25 +37,14 @@ int main()
 				   PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
 
 	arc::sensors::MPU6050 mpu6050{ 0x68 };
-	p_mpu6050 = &mpu6050;
 
 	sleep_ms(100);
 
-	mpu6050.sleep();
-
-#define PIN 9
-	gpio_init(PIN);
-	gpio_set_dir(PIN, GPIO_IN);
-	gpio_set_irq_enabled_with_callback(PIN, GPIO_IRQ_EDGE_RISE, true,
-					   &gpio_callback);
-	sleep_ms(100);
-
-	mpu6050.enableInterrupt();
-	mpu6050.wake();
-
+	float acc[3];
+	uint32_t counter = 0;
 	while (1) {
-		printf("Level: %d, counter: %d\n", gpio_get(PIN), counter);
-		sleep_ms(1000);
+		mpu6050.getAcc(acc);
+		sleep_ms(500);
+		printf("%u,%f,%f,%f\n", counter++, acc[0], acc[1], acc[2]);
 	}
-#endif
 }
