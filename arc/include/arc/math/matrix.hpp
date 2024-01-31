7d3ec6c99750694a68f1710e348a907a19d3e1de
AndreaMarcer <marcer.andrea@gmail.com>
2024-01-31 23:36:50 +0100

    [Matrix] Implement matrix class

---
+++ arc/include/arc/math/matrix.hpp
@@ -0,0 +1,260 @@
+/**
+ * @file constants.hpp
+ * @author Andrea Marcer (marcera.andrea@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-01-12
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#pragma once
+
+#include <type_traits>
+#include <initializer_list>
+
+#include "pico/stdlib.h"
+#include "common/log.hpp"
+
+namespace arc {
+namespace math {
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+class Matrix {
+    public:
+	static constexpr uint8_t m_cols = ROWS;
+	static constexpr uint8_t m_rows = COLS;
+
+	Matrix(); // Default
+	Matrix(const T &); // Default
+	Matrix(const T (&)[ROWS][COLS]); // Default
+	Matrix(const std::initializer_list<std::initializer_list<T> > &);
+	Matrix(const Matrix &); // copy
+	Matrix(Matrix &&); // move
+
+	~Matrix();
+
+	void print();
+
+	constexpr T *operator[](uint8_t n)
+	{
+		return m_matrix + n * COLS;
+	}
+
+	Matrix operator*(Matrix &);
+	Matrix operator*(T &&);
+	Matrix operator*(T &);
+
+	constexpr Matrix &operator=(const Matrix &);
+	constexpr Matrix &operator=(Matrix &&);
+
+	void clear();
+	void fill(const T &);
+
+    private:
+	T *m_matrix = new T[ROWS * COLS];
+};
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+Matrix<T, ROWS, COLS>::Matrix() // Default
+{
+	log_info("MATRIX Default\n");
+}
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+Matrix<T, ROWS, COLS>::Matrix(const T &v) // Default
+{
+	log_info("MATRIX Default with value\n");
+	fill(v);
+}
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+Matrix<T, ROWS, COLS>::Matrix(const Matrix &other) // copy
+{
+	log_info("MATRIX copy\n");
+	memcpy(m_matrix, other.m_matrix, COLS * ROWS * sizeof(T));
+}
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+Matrix<T, ROWS, COLS>::Matrix(Matrix &&other)
+	: m_matrix{ other.m_matrix } // move
+{
+	log_info("MATRIX move\n");
+	other.m_matrix = nullptr;
+}
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+Matrix<T, ROWS, COLS>::Matrix(
+	const std::initializer_list<std::initializer_list<T> > &matrix)
+{
+	log_info("MATRIX initializer_list\n");
+
+	auto it_r = matrix.begin();
+	for (uint8_t i = 0; i < ROWS && it_r != matrix.end(); ++i, ++it_r) {
+		auto it_c = it_r->begin();
+		for (uint8_t j = 0; j < COLS && it_c != it_r->end();
+		     ++j, ++it_c) {
+			m_matrix[i * COLS + j] = *it_c;
+		}
+	}
+}
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+Matrix<T, ROWS, COLS>::Matrix(const T (&matrix)[ROWS][COLS]) // Default
+{
+	log_info("MATRIX bidimensional array\n");
+	for (uint8_t i = 0; i < ROWS; i++) {
+		for (uint8_t j = 0; j < COLS; j++) {
+			m_matrix[i * COLS + j] = matrix[i][j];
+		}
+	}
+}
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+Matrix<T, ROWS, COLS>::~Matrix()
+{
+	log_info("MATRIX Default Destructor\n");
+	delete[] m_matrix;
+}
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+Matrix<T, ROWS, COLS> Matrix<T, ROWS, COLS>::operator*(Matrix &r_m)
+{
+	log_info("MATRIX moltiplication by matrix\n");
+
+	Matrix m;
+	for (uint8_t i = 0; i < ROWS; i++) {
+		for (uint8_t j = 0; j < COLS; j++) {
+			m[i][j] = m_matrix[i * COLS + j] * r_m[i][j];
+		}
+	}
+	return m;
+}
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+Matrix<T, ROWS, COLS> Matrix<T, ROWS, COLS>::operator*(T &&s)
+{
+	log_info("MATRIX moltiplication by r_value scalar\n");
+
+	Matrix m;
+	for (uint8_t i = 0; i < ROWS; i++) {
+		for (uint8_t j = 0; j < COLS; j++) {
+			m[i][j] = m_matrix[i * COLS + j] * s;
+		}
+	}
+	return m;
+}
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+Matrix<T, ROWS, COLS> Matrix<T, ROWS, COLS>::operator*(T &s)
+{
+	log_info("MATRIX moltiplication by reference scalar\n");
+
+	Matrix m;
+	for (uint8_t i = 0; i < ROWS; i++) {
+		for (uint8_t j = 0; j < COLS; j++) {
+			m[i][j] = m_matrix[i * COLS + j] * s;
+		}
+	}
+	return m;
+}
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+constexpr Matrix<T, ROWS, COLS> &
+Matrix<T, ROWS, COLS>::operator=(const Matrix<T, ROWS, COLS> &matrix)
+{
+	log_info("MATRIX assign reference\n");
+	if (this != &matrix) {
+		memcpy(m_matrix, &matrix, COLS * ROWS * sizeof(T));
+	}
+	return *this;
+}
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+constexpr Matrix<T, ROWS, COLS> &
+Matrix<T, ROWS, COLS>::operator=(Matrix<T, ROWS, COLS> &&other)
+{
+	log_info("MATRIX assign r_value\n");
+	m_matrix = other.m_matrix;
+	delete[] m_matrix;
+	other.m_matrix = nullptr;
+	return *this;
+}
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+void Matrix<T, ROWS, COLS>::clear()
+{
+	memset(m_matrix, 0, COLS * ROWS * sizeof(T));
+}
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+void Matrix<T, ROWS, COLS>::fill(const T &v)
+{
+	for (uint8_t i = 0; i < ROWS; i++) {
+		for (uint8_t j = 0; j < COLS; j++) {
+			m_matrix[i * COLS + j] = v;
+		}
+	}
+}
+
+template <typename T, uint8_t L_ROWS, uint8_t L_COLS_R_ROWS, uint8_t R_COLS>
+Matrix<T, L_ROWS, R_COLS> operator*(Matrix<T, L_ROWS, L_COLS_R_ROWS> l_m,
+				    Matrix<T, L_COLS_R_ROWS, R_COLS> r_m)
+{
+	log_info("MATRIX moltiplication [%hhu,%hhu] * [%hhu,%hhu] \n", L_ROWS,
+		 L_COLS_R_ROWS, L_COLS_R_ROWS, R_COLS);
+
+	Matrix<T, L_ROWS, R_COLS> m;
+	m.clear();
+	for (uint8_t i = 0; i < L_ROWS; i++) {
+		for (uint8_t j = 0; j < R_COLS; j++) {
+			for (uint8_t k = 0; k < L_COLS_R_ROWS; k++) {
+				m[i][j] += l_m[i][k] * r_m[k][j];
+			}
+		}
+	}
+	return m;
+}
+
+template <typename T, uint8_t ROWS, uint8_t COLS>
+void Matrix<T, ROWS, COLS>::print()
+{
+	if constexpr (std::is_integral_v<T>) {
+		if constexpr (std::is_unsigned_v<T>) {
+			for (uint8_t i = 0; i < ROWS; i++) {
+				log_debug("");
+				for (uint8_t j = 0; j < COLS; j++) {
+					log_debug_s("%u, ",
+						    m_matrix[i * COLS + j]);
+				}
+				log_debug_s("\n");
+			}
+		} else {
+			for (uint8_t i = 0; i < ROWS; i++) {
+				log_debug("");
+				for (uint8_t j = 0; j < COLS; j++) {
+					log_debug_s("%d, ",
+						    m_matrix[i * COLS + j]);
+				}
+				log_debug_s("\n");
+			}
+		}
+	} else {
+		if (std::is_floating_point_v<T>) {
+			for (uint8_t i = 0; i < ROWS; i++) {
+				log_debug("");
+				for (uint8_t j = 0; j < COLS; j++) {
+					log_debug_s("%f, ",
+						    m_matrix[i * COLS + j]);
+				}
+				log_debug_s("\n");
+			}
+		} else {
+			return;
+		}
+	}
+}
+
+} // arc
+} // math
--- arc/src/main.cpp
+++ arc/src/main.cpp
@@ -10,15 +10,20 @@
  */
 
 #include <stdio.h>
+
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "hardware/gpio.h"
-#include "sensors/MPU6050.hpp"
-#include "common/log.hpp"
-#include "common/common.hpp"
 #include "math.h"
 
+#include "common/log.hpp"
+#include "common/common.hpp"
+#include "common/geometry.hpp"
+#include "math/matrix.hpp"
+#include "sensors/MPU6050.hpp"
+
 using namespace arc::sensors;
+using namespace arc::common;
 
 uint32_t cnt = 0;
 bool new_data = false;
@@ -45,27 +50,89 @@ int main()
 	gpio_set_irq_enabled_with_callback(9, GPIO_IRQ_EDGE_RISE, true,
 					   &gpio_callback);
 
-	MPU6050 mpu6050{ i2c_default, MPU6050::I2C_ADDR_AD0_LOW };
-	mpu6050.setDLPFConfig(MPU6050::DlpfBW::_184Hz);
-	mpu6050.setAccRange(MPU6050::AccRange::_2G);
-	mpu6050.enableInterrupt();
-	mpu6050.wake();
+	log_info("=======================\n");
 
-	sleep_ms(100);
+	arc::math::Matrix<int, 2, 3> m1 = arc::math::Matrix<int, 2, 3>();
+	m1.print();
+	m1.clear();
+	m1.print();
 
-	arc::common::acc_t acc;
-	arc::common::acc_t gyro;
-	uint8_t tmp;
-	while (1) {
-		if (new_data) {
-			mpu6050.getAcc(acc.vec);
-			mpu6050.getGyro(gyro.vec);
-			printf("%u, %f,%f,%f %f,%f,%f %lu\n", cnt, acc.x, acc.y,
-			       acc.z, gyro.x, gyro.y, gyro.z,
-			       time_us_64() - interrupt_time);
-			new_data = false;
-			sleep_ms(1000);
-			mpu6050.getInterruptStatus(tmp);
-		}
-	}
+	arc::math::Matrix<uint, 3, 2> m2;
+	m2.print();
+
+	float arr[1][2] = { { 1, 2 } };
+	arc::math::Matrix m3(arr);
+	m3.print();
+
+	arc::math::Matrix<int, 2, 3> m4;
+	m4.clear();
+	m4.print();
+
+	arc::math::Matrix<int, 2, 3> m12{ { 1, 2, 3 }, { 2, 5, 6 } };
+	m12.print();
+
+	m4[1][1] = 5;
+
+	arc::math::Matrix m5(m4);
+	m5.print();
+
+	arc::math::Matrix m6 = m4 * m5;
+	m6.print();
+
+	arc::math::Matrix m7 = m6 * 2;
+	m7.print();
+
+	int num = 2;
+	arc::math::Matrix m8 = m6 * num;
+	m8.print();
+
+	m7 = m8 * 2;
+	m7.print();
+
+	arc::math::Matrix<int, 2, 3> m9(1);
+	m9.print();
+
+	arc::math::Matrix<int, 3, 2> m10(3);
+	m10.print();
+
+	arc::math::Matrix m11 = m9 * m10;
+	m11.print();
+
+	// arc::common::Vector<int, 3> v1 = { 2 };
+	// arc::common::Vector<int, 3> v2 = { 1 };
+	// arc::common::Vector<int, 3> v3;
+
+	// print(v1);
+	// print(v2);
+	// print(v3);
+
+	// if (v1 == v2) {
+	// 	printf("EQUAL\n");
+	// }
+	// v3 = v1 + v2;
+	// print(v3);
+
+	// MPU6050 mpu6050{ i2c_default, MPU6050::I2C_ADDR_AD0_LOW };
+	// mpu6050.setDLPFConfig(MPU6050::DlpfBW::_184Hz);
+	// mpu6050.setAccRange(MPU6050::AccRange::_2G);
+	// mpu6050.enableInterrupt();
+	// mpu6050.wake();
+
+	// sleep_ms(100);
+
+	// arc::common::acc_t acc;
+	// arc::common::acc_t gyro;
+	// uint8_t tmp;
+	// while (0) {
+	// 	if (new_data) {
+	// 		mpu6050.getAcc(acc.vec);
+	// 		mpu6050.getGyro(gyro.vec);
+	// 		printf("%u, %f,%f,%f %f,%f,%f %lu\n", cnt, acc.x, acc.y,
+	// 		       acc.z, gyro.x, gyro.y, gyro.z,
+	// 		       time_us_64() - interrupt_time);
+	// 		new_data = false;
+	// 		sleep_ms(1000);
+	// 		mpu6050.getInterruptStatus(tmp);
+	// 	}
+	// }
 }
